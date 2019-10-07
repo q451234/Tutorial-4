4bebcf31e5157360c4d514ef6d073962061f0adf
q451234 <1598576037@qq.com>
2019-10-07 21:52:58 +0800

    change

---
+++ src/main/java/tut/App.java
@@ -0,0 +1,13 @@
+package tut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/tut/Testcalc.java
@@ -0,0 +1,48 @@
+package tut;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Testcalc 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Testcalc( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Testcalc.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+
+
+    public void test() {
+	int result = Calc.add(1, 2);
+	assertEquals(3, result);
+		
+	}
+
+    public void test2() {
+	int result = Calc.subtract(2,1);
+    assertEquals(1, result);
+		
+	}
+
+}
